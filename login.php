fbde0c7f9c02d16ffae628e7efbbbd659c137245
nmakwana <nmakwana@atlassoftweb.com>
2017-02-24 12:06:18 +0530

    added login file

---
+++ login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+<form action="" method="post">
+	<input type="text" name="name"><br>
+	<input type="submit" name="submit">
+</form>
+</body>
+</html>
